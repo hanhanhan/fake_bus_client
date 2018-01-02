23bf29450b877870d54440c018f994121bbdd6c4
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2018-01-02 09:38:03 -0500

    update selection

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import socket from './Modules/APIConnection.js'
 import makeBusStopSelectionHtml from './Modules/BusStopSelectionHtml'
 
 
+
 let busStops = ["1", "2"]
 
 function update(schedule){
@@ -22,4 +23,9 @@ socket.on('schedule', (schedule) => {
   setInterval(update, 15000, schedule)
 })
 
-document.getElementById('bus-stop-selection').innerHTML = makeBusStopSelectionHtml()
+// on document load, set busStops = 1, 2 => trigger update of schedule
+const busStopsSelector = document.getElementById('bus-stop-selection')
+busStopsSelector.innerHTML = makeBusStopSelectionHtml()
+const busStopInputs = document.getElementsByClassName('bus-stop')
+    .foreach(console.log(item))
+// busStopsSelectors.onchange = event.target => {console.log(event.target)}
